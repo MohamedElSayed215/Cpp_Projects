8402c4387752efc11bc45ff20443993c3ffb6dc2
Mohamed ElSayed <mohamedelsayedmahmoud21@gmail.com>
2024-10-09 01:51:37 +0300

    Delete ProjectServices.hpp

--- ProjectServices.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __PROJECTSERVICES__H__
-#define __PROJECTSERVICES__H__
-
-#include "WIFI.hpp"
-#include "Bluetooth.hpp"
-#include <unistd.h>
- extern void (*DisplayService[2][8])() ; 
-
-#endif  //!__PROJECTSERVICES__H__
